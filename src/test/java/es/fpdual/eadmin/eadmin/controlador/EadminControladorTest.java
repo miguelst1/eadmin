150edd7b47760d865736a92e60ef9c137950e93a
miguelst1 <miguel_s_t1@hotmail.com>
2018-04-04 11:48:00 +0200

    Commit miercoles 4 4

---
+++ src/test/java/es/fpdual/eadmin/eadmin/controlador/EadminControladorTest.java
@@ -0,0 +1,48 @@
+package es.fpdual.eadmin.eadmin.controlador;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+import es.fpdual.eadmin.eadmin.servicios.ServicioDocumento;
+
+public class EadminControladorTest {
+
+	private EadminControlador eadminControlador;
+	private ServicioDocumento servicioDocumento = mock(ServicioDocumento.class);
+	private Documento documento1 = mock(Documento.class);
+
+	@Before
+	public void inicializarVariables() {
+		this.eadminControlador = spy(new EadminControlador(servicioDocumento));
+	}
+
+	@Test
+	public void testGetDocumento() {
+		when(documento1.getCodigo()).thenReturn(1);
+		doReturn(documento1).when(servicioDocumento).obtenerDocumentoPorCodigo(1);
+
+		Documento documentoControlador = eadminControlador.getDocumento(1).getBody();
+
+		assertSame(documento1, documentoControlador);
+	}
+
+	@Test
+	public void testGetTodosDocumentos() {
+		final List<Documento> lista = new ArrayList<>();
+		doReturn(lista).when(servicioDocumento).obtenerTodosLosDocumentos();
+
+		List<Documento> listaControlador = eadminControlador.getTodosDocumentos().getBody();
+
+		assertSame(lista, listaControlador);
+	}
+}
